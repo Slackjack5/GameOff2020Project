26035eb06a2475a3e4d8a39dd8d55952767fa91f
Aljon Pineda <aljonjbpineda@gmail.com>
2020-11-08 19:49:41 -0500

    Create pause menu script

---
+++ GameOff2020Unity/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+
+    private bool gameIsPaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    private void TogglePause()
+    {
+        if (gameIsPaused)
+        {
+            Resume();
+        } else
+        {
+            Pause();
+        }
+    }
+
+    private void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
