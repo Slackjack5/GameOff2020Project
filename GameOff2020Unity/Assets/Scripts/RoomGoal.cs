58c48fbeb70b817e8862f0a6dc78f63b914fab25
Aljon Pineda <aljonjbpineda@gmail.com>
2020-11-09 19:21:21 -0500

    Create script for room goal

---
+++ GameOff2020Unity/Assets/Scripts/RoomGoal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class RoomGoal : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private UnityEvent onExit = new UnityEvent();
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            onExit.Invoke();
+        }
+    }
+}
