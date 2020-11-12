0235cb3f5caf80b25315bf4adc81c0a70ad1215e
Aljon Pineda <aljonjbpineda@gmail.com>
2020-11-11 20:10:19 -0500

    Have the dolly camera just pan to the right when the player exits a room

--- GameOff2020Unity/Assets/Scripts/CameraManager.cs
+++ GameOff2020Unity/Assets/Scripts/CameraManager.cs
@@ -7,11 +7,8 @@ public class CameraManager : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera dollyVcam;
     [SerializeField] private CinemachineSmoothPath dollyTrack;
-    [SerializeField] private float cameraMoveSpeed = 0.05f;
-    [SerializeField] private float zoomOutDistance = 40f;
+    [SerializeField] private float cameraMoveSpeed = 0.25f;
     [SerializeField] private List<Transform> roomTransforms;
-    [SerializeField] private float leftWaypointOffset = 9f;   // Represents how far left the waypoint preceding the waypoint of the target room should be
-    [SerializeField] private float rightWaypointOffset = 6f;  // Represents how far right the waypoint following the waypoint of the exited room should be
 
     private CinemachineTrackedDolly dolly;
     private float initialPositionZ;
@@ -33,32 +30,12 @@ public class CameraManager : MonoBehaviour
         {
             float currentRoomPositionX = roomTransforms[i].position.x;
 
-            // Each room except the first room has a waypoint to the left of it
-            if (i > 0)
-            {
-                CinemachineSmoothPath.Waypoint leftWaypoint = new CinemachineSmoothPath.Waypoint
-                {
-                    position = new Vector3(currentRoomPositionX - leftWaypointOffset, dollyVcam.transform.position.y, initialPositionZ - zoomOutDistance)
-                };
-                waypoints.Add(leftWaypoint);
-            }
-
             // Every room has a waypoint at its center
             CinemachineSmoothPath.Waypoint centerWaypoint = new CinemachineSmoothPath.Waypoint
             {
                 position = new Vector3(currentRoomPositionX, dollyVcam.transform.position.y, initialPositionZ)
             };
             waypoints.Add(centerWaypoint);
-
-            // Each room except the last room has a waypoint to the right of it
-            if (i < roomTransforms.Count - 1)
-            {
-                CinemachineSmoothPath.Waypoint rightWaypoint = new CinemachineSmoothPath.Waypoint
-                {
-                    position = new Vector3(currentRoomPositionX + rightWaypointOffset, dollyVcam.transform.position.y, initialPositionZ - zoomOutDistance)
-                };
-                waypoints.Add(rightWaypoint);
-            }
         }
 
         dollyTrack.m_Waypoints = waypoints.ToArray();
@@ -68,16 +45,13 @@ public class CameraManager : MonoBehaviour
     {
         if (isExiting)
         {
-            // There are 3 waypoints to get from one room to the next
-            int targetPathPosition = 3 * targetRoomIndex;
-
-            if (dolly.m_PathPosition < targetPathPosition)
+            if (dolly.m_PathPosition < targetRoomIndex)
             {
                 dolly.m_PathPosition += cameraMoveSpeed;
             }
             else
             {
-                dolly.m_PathPosition = targetPathPosition;
+                dolly.m_PathPosition = targetRoomIndex;
                 isExiting = false;
             }
         }
