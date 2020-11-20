34150ea2ec782fd2249fac66ca96276731baae2a
Aljon Pineda <aljonjbpineda@gmail.com>
2020-11-20 13:04:42 -0500

    Create a floating enemy that shoots small lasers

---
+++ GameOff2020Unity/Assets/Scripts/Enemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private EnemyLaser laser;
+    [SerializeField] private float shootSpeed = 12f;
+    [Range(-180f, 180f)] [SerializeField] private float shootAngle = -90f;  // Angle relative to the positive x-axis
+    [SerializeField] private float shootOffset = 0.5f;                      // Specifies how far from the enemy the projectile spawns
+    [SerializeField] private float cooldown = 1.2f;                         // Specifies how long the enemy waits before shooting another projectile
+
+    private float cooldownTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cooldownTime = cooldown;
+    }
+
+    private void FixedUpdate()
+    {
+        cooldownTime -= Time.fixedDeltaTime;
+
+        if (cooldownTime <= 0)
+        {
+            Shoot();
+            cooldownTime = cooldown;
+        }
+    }
+
+    private void Shoot()
+    {
+        // Spawn a projectile at a position near the enemy based on the shootOffset,
+        // and rotate it based on the shootAngle.
+        Quaternion rotation = Quaternion.AngleAxis(shootAngle, Vector3.forward);
+        Vector3 spawnPosition = transform.position + rotation * Vector3.right * shootOffset;
+        EnemyLaser shotLaser = Instantiate(laser, spawnPosition, rotation);
+        shotLaser.SetShootSpeed(shootSpeed);
+    }
+}
---
+++ GameOff2020Unity/Assets/Scripts/EnemyLaser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLaser : MonoBehaviour
+{
+    private float shootSpeed;
+    private bool hit = false;
+
+    public void SetShootSpeed(float shootSpeed)
+    {
+        this.shootSpeed = shootSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDistance = transform.right * shootSpeed * Time.fixedDeltaTime;
+
+        if (hit)
+        {
+            // Shrink the laser as it gets absorbed into the thing it collided with
+            transform.localScale = new Vector2(transform.localScale.x - shootSpeed * Time.fixedDeltaTime, transform.localScale.y);
+            transform.position += moveDistance / 2;
+
+            if (transform.localScale.x <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            transform.position += moveDistance;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        hit = true;
+    }
+}
