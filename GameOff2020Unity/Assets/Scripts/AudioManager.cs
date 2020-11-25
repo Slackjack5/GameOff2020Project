88dba7afa52f58bad9592400e50d9b8afba5e876
Nycelly <dkr264@nyu.edu>
2020-11-24 19:27:44 -0500

    No Yellow

--- GameOff2020Unity/Assets/Scripts/AudioManager.cs
+++ GameOff2020Unity/Assets/Scripts/AudioManager.cs
@@ -8,9 +8,6 @@ public class AudioManager : MonoBehaviour
 
     public Sound[] sounds;
 
-    private bool fadeOut = false;
-    private float sVolume = 0;
-
     public static AudioManager instance;
     // Start is called before the first frame update
     void Awake()
