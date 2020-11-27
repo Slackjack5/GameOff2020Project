7c75227cefe8bf059e18a880b79fc57405103f6e
Aljon Pineda <aljonjbpineda@gmail.com>
2020-11-27 16:33:12 -0500

    Add initial countdown

--- GameOff2020Unity/Assets/Scripts/Timer.cs
+++ GameOff2020Unity/Assets/Scripts/Timer.cs
@@ -5,28 +5,54 @@ using UnityEngine.UI;
 using TMPro;
 public class Timer : MonoBehaviour
 {
+    [SerializeField, Range(0f, 1f)] private float countdownSecond = 1.0f;  // Specifies how long it takes to decrement the countdown by 1
+    [SerializeField] private TextMeshProUGUI countdownText;
+    
+    private TextMeshProUGUI timerText;
+
     private float timeElapsed = 0;
     private bool timerIsRunning = false;
-
-    private TextMeshProUGUI timeText;
+    private bool countingDown = true;
+    private int countdownTime = 3;       // What is displayed
+    private float currentCountdownTime;  // Represents how long before countdownTime decrements by 1
 
     private void Start()
     {
-        timerIsRunning = true;
-        timeText = gameObject.GetComponent<TextMeshProUGUI>();
+        timerText = gameObject.GetComponent<TextMeshProUGUI>();
+
+        currentCountdownTime = countdownSecond;
     }
+
     private void Update()
     {
+        DisplayTime(timeElapsed);
+
+        if (countdownTime <= 0)
+        {
+            // Countdown is over
+            StartCoroutine(DisplayGo());
+            countingDown = false;
+            timerIsRunning = true;
+        }
+        else if (countingDown)
+        {
+            DisplayCountdownTime(countdownTime);
+
+            currentCountdownTime -= Time.deltaTime;
+            if (currentCountdownTime <= 0)
+            {
+                countdownTime--;
+                currentCountdownTime = countdownSecond;
+            }
+        }
+
         if (timerIsRunning)
         {
             timeElapsed += Time.deltaTime;
-            DisplayTime(timeElapsed);
         }
         else
         {
-            Debug.Log("Time has run out!");
             timeElapsed = 0;
-            timerIsRunning = false;
         }
     }
 
@@ -36,7 +62,18 @@ public class Timer : MonoBehaviour
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
         float milliSeconds = Mathf.FloorToInt((timeToDisplay * 100) % 100);
 
-        timeText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliSeconds);
+        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, milliSeconds);
     }
 
+    private void DisplayCountdownTime(int time)
+    {
+        countdownText.text = string.Format("{0}", time);
+    }
+
+    private IEnumerator DisplayGo()
+    {
+        countdownText.text = "GO";
+        yield return new WaitForSeconds(countdownSecond);
+        countdownText.enabled = false;
+    }
 }
