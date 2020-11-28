d8916856fa0e1d9299ddb9fa60c1dda2f74f914e
Aljon Pineda <aljonjbpineda@gmail.com>
2020-11-28 00:05:15 -0500

    Add door that only opens when all enemies are killed

---
+++ GameOff2020Unity/Assets/Scripts/Door.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private List<Enemy> enemies;
+
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AllEnemiesDead())
+        {
+            spriteRenderer.enabled = false;
+            boxCollider.enabled = false;
+        }
+        else
+        {
+            spriteRenderer.enabled = true;
+            boxCollider.enabled = true;
+        }
+    }
+
+    private bool AllEnemiesDead()
+    {
+        foreach (Enemy enemy in enemies)
+        {
+            if (!enemy.dead)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
