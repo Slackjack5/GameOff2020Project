c692a13c7446885637460cee0d61ab79bb04ace1
Aljon Pineda <aljonjbpineda@gmail.com>
2020-11-29 03:48:03 -0500

    Fix laser going through walls

--- GameOff2020Unity/Assets/Scripts/PlayerLaser.cs
+++ GameOff2020Unity/Assets/Scripts/PlayerLaser.cs
@@ -12,12 +12,10 @@ public class PlayerLaser : Laser
         Vector2 transformPosition = transform.position;
         Vector2 firstLaserVector = firstLaserHit.point - transformPosition;
         Vector2 reflectedVector = Vector2.Reflect(firstLaserVector, firstLaserHit.normal);
-        RaycastHit2D secondLaserHit = LaserHit(firstLaserHit.point, reflectedVector.normalized);
+        RaycastHit2D secondLaserHit = LaserHit(firstLaserHit.point, reflectedVector.normalized, true);
         lineRenderer.SetPosition(2, secondLaserHit.point);
 
         edgeColliderPoints.Add(edgeCollider.transform.InverseTransformPoint(secondLaserHit.point));
         edgeCollider.points = edgeColliderPoints.ToArray();
-
-
     }
 }
