7f447504d9424d3ab23874661dd65055f27265d5
Nycelly <dkr264@nyu.edu>
2020-11-28 21:57:55 -0500

    Finished Background Scrolling

---
+++ GameOff2020Unity/Assets/Scripts/RenderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Add a namespace?
+public static class RenderExtensions
+{
+    public static bool IsVisibleFrom(this Renderer renderer, Camera camera)
+    {
+        Plane[] planes = GeometryUtility.CalculateFrustumPlanes(camera);
+        return GeometryUtility.TestPlanesAABB(planes, renderer.bounds);
+    }
+
+}
