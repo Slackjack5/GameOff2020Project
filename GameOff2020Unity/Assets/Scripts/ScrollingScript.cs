34b3fa068737436eec0b87303bd1a9411c7432d2
Nycelly <dkr264@nyu.edu>
2020-11-28 21:33:34 -0500

    Parallax background , Not infinite yet

---
+++ GameOff2020Unity/Assets/Scripts/ScrollingScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(2, 2);
+
+    public Vector2 direction = new Vector2(-1, 0);
+
+    public bool isLinkedToCamera = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement
+        Vector3 movement = new Vector3(
+          speed.x * direction.x,
+          speed.y * direction.y,
+          0);
+
+        movement *= Time.deltaTime;
+        transform.Translate(movement);
+    }
+}
